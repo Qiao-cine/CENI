c1580aa81b8f63d2471ce86f03396c906d605f97
Qiao-cine <qiaolu@contractor.ciena.com>
2019-10-31 11:59:05 -0400

    plotly added

--- client/views/network-insight/network-insight.js
+++ client/views/network-insight/network-insight.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 
+import Plotly from ‘plotly.js-basic-dist’;
+import createPlotlyComponent from 'react-plotly.js/factory';
+const Plot = createPlotlyComponent(Plotly);
+
+
 class networkInsight extends Component {
 
   constructor(props) {
@@ -13,6 +18,20 @@ class networkInsight extends Component {
   render() {
       return (
        <div>hello</div>
+      <Plot
+        data={[
+          {
+            x: [1, 2, 3],
+            y: [2, 6, 3],
+            type: 'scatter',
+            mode: 'lines+markers',
+            marker: {color: 'red'},
+          },
+          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+        ]}
+        layout={{width: 320, height: 240, title: 'A Fancy Plot'}}
+      />
+
       );
     }
 
